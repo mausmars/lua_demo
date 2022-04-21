b13a70a3c67d2536e11bfbe19a9147afba638d17
马里 <dalaoshuxxx@gmail.com>
2022-04-21 21:13:04 +0800

    提交 lua 调用c 测试。。。

--- src/lua2c_test/test_lib2.lua
+++ src/lua2c_test/test_lib2.lua
@@ -4,11 +4,11 @@ local ctest = require "ctest"           -- 对应luaopen_test中的myLualib
 local function test()
     local a = ctest.new(1000)
     print(a)                        --> userdata: 0x8064d48
-    print(ctest.size(a))            --> 1000
+    print("size=" .. ctest.size(a))
     for i = 1, 1000 do
-        ctest.set(a, i, 1 / i)
+        ctest.set(a, i, i)
     end
-    print(ctest.get(a, 10))         --> 0.1
+    print("get v=" .. ctest.get(a, 10))
 end
 
 test()
