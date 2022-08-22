00a34f1cf263c0b67cd5605da558fded9de16533
马里 <dalaoshuxxx@gmail.com>
2022-08-22 18:20:50 +0800

    测试lua回调。。。

---
+++ src/c_cb_lua/test.lua
@@ -0,0 +1,35 @@
+package.cpath = "./?.so" --so搜寻路劲
+local cb = require("cb")
+
+local function callback(a, b, c)
+    print("callback a=" .. a .. " b=" .. b .. " c=" .. c)
+end
+
+function defcallback(a, b, c)
+    print("defcallback a=" .. a .. " b=" .. b .. " c=" .. c)
+end
+
+local function watch(a, b, c,d)
+    print("### lua watch a=" .. a .. " b=" .. b .. " c=" .. c)
+end
+
+local function task()
+    cb.setwatch2(watch)
+    print("task over")
+end
+
+print "---------------------------"
+local co = coroutine.create(task)
+coroutine.resume(co)
+co = nil
+print "---------------------------"
+--cb.setwatch(watch)
+print "---------------------------"
+--cb.setnotify(callback)
+--cb.testnotify()
+print "---------------------------"
+--cb.testenv()
+print "---------------------------"
+
+os.execute("sleep " .. 10)
+print "test over!"
